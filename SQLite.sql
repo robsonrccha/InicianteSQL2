8d2280bc7854bd58224ac950475a5f50f173906d
Robson Rocha <135916984+robsonrccha@users.noreply.github.com>
2024-04-07 15:27:08 -0300

    Att SQLite.sql

--- SQLite.sql
+++ SQLite.sql
@@ -1,5 +1,5 @@
 -- Consulta 1: Retornar a média de Notas dos Alunos em história.
-select AVG(valor_nota) FROM tabelanotas;
+SELECT AVG(valor_nota) FROM tabelanotas;
 
 -- Consulta 2: Retornar as informações dos alunos cujo Nome começa com 'A'.
 -- SELECT nome_aluno FROM tabelaalunos;
@@ -17,3 +17,13 @@ SELECT id_aluno, nome_aluno, ROUND((JULIANDAY('now') - JULIANDAY(data_nascimento
 FROM tabelaalunos;
 
 -- Consulta 5: Retornar se o aluno está ou não aprovado. Aluno é considerado aprovado se a sua nota foi igual ou maior que 6
+-- SELECT id_nota, valor_nota FROM tabelanotas;
+SELECT id_aluno, valor_nota,
+CASE
+WHEN valor_nota >= 6 THEN 'Aprovado'
+ELSE 'Reprovado'
+END AS 'Resultado'
+FROM tabelanotas
+ORDER BY valor_nota DESC;
+
+-- .
